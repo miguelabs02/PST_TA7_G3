67a00bbcdf05a9a7db921672e80c95e2ee05958b
miguelabs02 <miguel_alejob@hotmail.com>
2019-08-04 13:15:29 -0500

    Correcion Manifest,Uso de librerias AndroidX, RegisterActivity creada

--- app/src/main/java/com/example/soportetecnico/RegisterActivity.java
+++ app/src/main/java/com/example/soportetecnico/RegisterActivity.java
@@ -1,13 +1,14 @@
 package com.example.soportetecnico;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class UserActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cliente);
+        setContentView(R.layout.activity_register);
     }
 }

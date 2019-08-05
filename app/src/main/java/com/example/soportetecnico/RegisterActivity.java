0800fd3411aa2ebd86f6a139f0a52d69127d8346
miguelabs02 <miguel_alejob@hotmail.com>
2019-08-04 19:02:37 -0500

    Base de datos creada y Perfil de Usuario xml

--- app/src/main/java/com/example/soportetecnico/RegisterActivity.java
+++ app/src/main/java/com/example/soportetecnico/RegisterActivity.java
@@ -1,14 +1,36 @@
 package com.example.soportetecnico;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+public class RegisterActivity extends SQLiteOpenHelper {
+
+    public RegisterActivity(Context context, String name,
+                            SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table usuarios(usuario text primary key, contraseña text," +
+                "nombres text, apellidos text, fecha_nacimiento text, producto text)");
     }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    /**
+     * Se utiliza el formato de fecha iso8601Format yyyy-MM-dd HH:mm:ss
+     * @param context
+     * @param fecha
+     * @return
+     */
+
 }
